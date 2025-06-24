ed3e0d1693063716d5925ece05c381e5aaa16183
ragnos6 <ragnos@ya.ru>
2025-06-24 21:28:20 +0300

    update

--- project/settings.py
+++ project/settings.py
@@ -91,7 +91,7 @@ DATABASES = {
         "NAME": os.environ.get("POSTGRES_DB"),
         "USER": os.environ.get("POSTGRES_USER"),
         "PASSWORD": os.environ.get("POSTGRES_PASSWORD"),
-        "HOST": os.environ.get("POSTGRES_HOST", 'db'),   
+        "HOST": os.environ.get("POSTGRES_HOST", 'localhost'),   
         "PORT": os.environ.get("POSTGRES_PORT", '5432'),
     }
 }
